f853ff5d4c180927be8d9db6fd87b8a8fd10bb33
sole <spenades@mozilla.com>
2018-11-05 11:46:44 +0000

    liberally using dump as advised by Yulia
    MozReview-Commit-ID: Be5TRAP1uJZ

--- devtools/client/webaudioeditor/test/browser_wa_properties-view-media-nodes.js
+++ devtools/client/webaudioeditor/test/browser_wa_properties-view-media-nodes.js
@@ -42,15 +42,19 @@ add_task(async function() {
   Services.prefs.setBoolPref(MEDIA_PERMISSION, true);
 
   await loadFrameScriptUtils();
-
+dump("\nSOLE!1\n");
   const events = Promise.all([
     getN(gFront, "create-node", 4),
     waitForGraphRendered(panelWin, 4, 0),
   ]);
+dump("\nSOLE!2\n");
   reload(target);
+dump("\nSOLE!3\n");
   const [actors] = await events;
-  const nodeIds = actors.map(actor => actor.actorID);
+dump("\nSOLE!4\n");
+  const nodeIds = actors.map(actor => actor.actorID); // SOLE: could the nativeToActorID is null message come from here?
 
+dump("\nSOLE!5\n");
   const types = [
     "AudioDestinationNode", "MediaElementAudioSourceNode",
     "MediaStreamAudioSourceNode", "MediaStreamAudioDestinationNode",
@@ -58,16 +62,24 @@ add_task(async function() {
 
   const defaults = await Promise.all(types.map(type => nodeDefaultValues(type)));
 
+dump("\nSOLE!6\n");
   for (let i = 0; i < types.length; i++) {
     click(panelWin, findGraphNode(panelWin, nodeIds[i]));
+dump("\nSOLE!7 - " + i + "\n");
     await waitForInspectorRender(panelWin, EVENTS);
+dump("\nSOLE!8 - " + i + "\n");
     checkVariableView(gVars, 0, defaults[i], types[i]);
+dump("\nSOLE!9 - " + i + "\n");
   }
 
+dump("\nSOLE!10\n");
   // Reset permissions on getUserMedia
   Services.prefs.setBoolPref(MEDIA_PERMISSION, mediaPermissionPref);
 
+dump("\nSOLE!11\n");
   await teardown(target);
 
+dump("\nSOLE!12\n");
   await waitForDeviceClosed();
+dump("\nSOLE!13\n");
 });
